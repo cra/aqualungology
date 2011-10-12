a8bf3735a47aef3f2a84059212a780bf2941139d
Igor Mosyagin <cramur@mail.ru>
2011-10-12 04:13:10 +0400

    fix stupid bugs

--- blag/content/models.py
+++ blag/content/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class MediaEntry(models.Model):
     name = models.CharField(u'Название', max_length=200)
     reason = models.TextField(u'Причина добавления', max_length=400, default='')
-    processed = models.BoolField(u'Усвоено')
-    emotions = models.TextField(u'Впечатления', max_length=300, default='', default=False)
+    processed = models.BooleanField(u'Усвоено')
+    emotions = models.TextField(u'Впечатления', max_length=300, default=False)
 
     class Meta:
         verbose_name = u'Ресурс'
@@ -21,7 +21,7 @@ class MediaEntry(models.Model):
 
 class Film(MediaEntry):
     FILM_GENRES = (
-        ('U', 'Unspecified')
+        ('U', 'Unspecified'),
         ('O', 'Other'),
         ('D', 'Documentary'),
         ('S', 'Series'),
@@ -29,6 +29,6 @@ class Film(MediaEntry):
         ('C', 'Comedy'),
     )
 
-    length = models.IntegerField(u'Длительность в минутах', required=False)
-    genre = models.CharField(max_length=1, choices=FILM_GENRES, required=False)
-    company = models.CharField(u'Компания для просмотра', required=False)
+    length = models.CharField(u'Длительность в минутах', max_length=20,  blank=True)
+    genre = models.CharField(max_length=1, choices=FILM_GENRES, blank=True)
+    company = models.CharField(u'Компания для просмотра', max_length=100, blank=True)
