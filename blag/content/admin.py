9e833c8ea6e8f98bdaee47292e92870ded9182a6
Igor Mosyagin <cramur@mail.ru>
2012-02-21 20:06:14 +0400

    is_draft -> is_published

--- blag/content/admin.py
+++ blag/content/admin.py
@@ -1,8 +1,17 @@
 from content.models import MediaEntry, Article
 from django.contrib import admin
 
+def publish(modeladmin, request, queryset):
+    queryset.update(is_published=True)
+publish.short_description = "Publish selected articles"
+
+def unpublish(modeladmin, request, queryset):
+    queryset.update(is_published=False)
+unpublish.short_description = "Mark selected articles as draft"
+
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('slug', 'title', 'date_published', 'is_draft')
+    list_display = ('slug', 'title', 'date_published', 'is_published')
+    actions = [publish, unpublish]
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(MediaEntry)
