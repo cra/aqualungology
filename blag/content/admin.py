30f996ac6a328352b1e2f2056faff87f9a7c8c6b
Igor Mosyagin <cramur@mail.ru>
2012-02-21 19:52:32 +0400

    :))

--- blag/content/admin.py
+++ blag/content/admin.py
@@ -2,7 +2,7 @@ from content.models import MediaEntry, Article
 from django.contrib import admin
 
 class ArticleAdmin(admin.ModelAdmin):
-    fields = ['slug', 'title', 'date_published', 'is_draft']
+    list_display = ('slug', 'title', 'date_published', 'is_draft')
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(MediaEntry)
