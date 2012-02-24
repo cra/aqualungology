5185cfad0d96708ab9078ce1b5bd2f396b2f9955
Igor Mosyagin <cramur@mail.ru>
2012-02-24 18:14:04 +0400

    локальные пути для staticfiles

--- blag/urls.py
+++ blag/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,3 +12,4 @@ urlpatterns = patterns(
 
     url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += staticfiles_urlpatterns()
