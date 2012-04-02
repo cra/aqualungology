25a5fb7d5e5318fe1bce688c3412cd076400fd2f
Igor Mosyagin <cramur@mail.ru>
2012-04-02 18:40:25 +0400

    content вместо description

--- blag/content/feeds.py
+++ blag/content/feeds.py
@@ -8,14 +8,14 @@ from content.models import Article
 class LatestEntriesFeed(Feed):
     title = "cramur.me posts"
     link = "/feed/"
-    description = "My blog entries"
+    description = "cramur.me blog entries. Updates rarely"
 
     def items(self):
         return Article.objects.filter(is_published=True).order_by('-date_published')[:7]
 
     def item_title(self, item):
-        return item.title
+        return item.title + u" -- %s" % item.summary
 
     def item_description(self, item):
-        return item.summary
+        return item.content
 
