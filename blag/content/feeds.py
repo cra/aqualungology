7e1267151d3eab7e9b919f6584b8d1901964c40c
Igor Mosyagin <cramur@mail.ru>
2012-10-03 18:53:33 +0200

    Пофиксил дату в фиде

--- blag/content/feeds.py
+++ blag/content/feeds.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 
 from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Atom1Feed
+from django.utils.safestring import mark_safe
 
 from content.models import Article
 
@@ -9,13 +11,22 @@ class LatestEntriesFeed(Feed):
     title = "cramur.me posts"
     link = "/feed/"
     description = "cramur.me blog entries. Updates rarely"
+    feed_type = Atom1Feed
+    item_author_name = "cra"
+    item_author_email = "c6h10o5@gmail.com"
+    feed_copyright = "Copyright (c) WTFPL, cra"
+    #title_template = "feeds/latest_title.html"
+    #description_template = "feeds/latest_description.html"
 
     def items(self):
         return Article.objects.filter(is_published=True).order_by('-date_published')[:7]
 
     def item_title(self, item):
-        return item.title + u" -- %s" % item.summary
+        return mark_safe(item.title + u" &mdash; %s" % item.summary)
 
     def item_description(self, item):
-        return item.content
+        return mark_safe(item.content)
+
+    def item_pubdate(self, item):
+        return item.date_published
 
