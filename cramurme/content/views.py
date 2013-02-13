963ee5f95c713dc56c46ac89470cd8db6a14ac47
Igor Mosyagin <cramur@mail.ru>
2013-02-13 17:21:40 +0100

    Drag out chronolist for articles. Put on startpage

--- cramurme/content/views.py
+++ cramurme/content/views.py
@@ -8,13 +8,22 @@ from django.shortcuts import redirect, get_object_or_404
 from content.models import Film, Article
 from content.forms import AddFilmForm
 
+def get_articles_by_year_dict():
+    all_articles = Article.objects.all()
+    years = [a.date_published.year for a in all_articles]
+
+    articles = dict()
+    for year in set(years):
+        articles[year] = all_articles.filter(date_published__year=year).order_by('date_published')
+
+    return articles
+
 
 @render_to('index.html')
 def index(request):
     article = Article.objects.filter(is_published=True).latest('date_published')
-    all_articles = Article.objects.all()
     return {'article' : article,
-            'all_articles': all_articles,}
+            'articles': get_articles_by_year_dict(),}
 
 
 @render_to('upload.html')
@@ -33,17 +42,7 @@ def article_details(request, slug):
 
 @render_to('articles.html')
 def articles(request):
-    ctx = dict()
-
-    all_articles = Article.objects.all()
-    #years = sorted([a.date_published.year for a in all_articles])
-    years = [a.date_published.year for a in all_articles]
-
-    ctx['articles'] = dict()
-    for year in set(years):
-        ctx['articles'][year] = all_articles.filter(date_published__year=year).order_by('date_published')
-
-    return ctx
+    return {'articles': get_articles_by_year_dict() }
 
 
 @render_to('about.html')
