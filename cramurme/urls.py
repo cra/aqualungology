39070067649e73cd87bc613199d5886d7cc7e95e
Igor Mosyagin <cramur@mail.ru>
2013-02-14 17:22:57 +0100

    Simple stats.txt viewer. OLOLO

--- cramurme/urls.py
+++ cramurme/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
@@ -8,6 +9,8 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^$', 'content.views.index', name='index'),
+
+    url(r'^stats/$', direct_to_template, {'template': 'stats.txt'}),
 
     url(r'^admin/', include(admin.site.urls)),
 
